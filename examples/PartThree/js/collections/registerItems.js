b1a19ce5a7fce9662c9ebb4918c4ed548741e077
peej <2kittymafia@gmail.com>
2012-04-11 15:48:23 -0500

    fixin url

--- examples/PartThree/js/collections/registerItems.js
+++ examples/PartThree/js/collections/registerItems.js
@@ -1,7 +1,7 @@
 define(['backbone', 'models/registerItem'],function(Backbone, RegisterItem){
     var collection = Backbone.Collection.extend({
                                                     model: RegisterItem,
-                                                    url: '/registeritems.json'
+                                                    url: '/BackboneForNoobs/registeritems.json'
                                                 });
 
     return collection;
